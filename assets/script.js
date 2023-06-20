f5527988c3ccb7833f5aab4c240bca23d8de81aa
Camille MacLaren <camillemaclaren@gmail.com>
2023-06-20 05:17:17 -0500

    testing getting apis does not work

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,69 @@
 
 var key = 'c8502ebcdb7a83fd03a62d0aaaafa07c';
-var city = 'https://api.openweathermap.org/data/2.5/weather?q=Houston,Texas&appid='+ key;
-//var location = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid='+key;
-getApi();
+var coordURL = 'https://api.openweathermap.org/data/2.5/weather?q=Houston,Texas&appid='+ key;
 
-function getApi() 
+var lat, lon;
+
+coord = getCoord();
+console.log(coord  );
+function getCoord()
 {
-  
-    fetch(city)
-      .then(function (response) 
-      {
-        return response.json();
-      })
-      .then(function (data) 
-      {
-        console.log(data);
-      });
-}
+    var coord = {lat: lat, lon: lon};
+    $.ajax(
+    {
+        url: coordURL,
+        type: "POST",
+        data: JSON.stringify(coord),
+        success: function(response)
+        {
+            
+        },
+        error: function(error)
+        {
+            console.log(error)
+        }
+            
+    })
+    .then(function (response)
+    {
+        console.log(response);
+    });
+    console.log(coord  );
+ }
+
+    // console.log('lat city: '+ lat);
+// console.log('lon city: '+ lon);
+// function getApi() 
+// {
+//     //get the city's latitude and longitude
+//     return fetch(coordURL)
+//       .then(function (response) 
+//       {
+//         return response.json();
+//       })
+//       .then(function (data) 
+//       {
+//         console.log(data);
+//         return data.coord;
+        
+//       });
+    
+// }
+
+//   lat = data.coord.lat;
+    // lon = data.coord.lon;
+
+    // get the city's weather info
+    // var  cityURL = 'https://api.openweathermap.org/data/2.5/weather?lat='
+    //                 + lat + '&lon='+ lon +'&appid='+ key;
+    
+    // fetch(cityURL)
+    //   .then(function (response) 
+    //   {
+    //     return response.json();
+    //   })
+    //   .then(function (data) 
+    //   {
+    //     console.log(data);
+        
+    //   })
